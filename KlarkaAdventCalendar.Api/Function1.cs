9c50427d1c4e6daa725047b04b796ebb60ca1946
Jan Klicpera <honza.klicpera@gmail.com>
2024-12-02 22:10:00 +0100

    Add email support

--- KlarkaAdventCalendar.Api/Function1.cs
+++ KlarkaAdventCalendar.Api/Function1.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Azure;
+using Azure.Communication.Email;
 
 namespace KlarkaAdventCalendar.Api
 {
@@ -15,9 +17,30 @@ namespace KlarkaAdventCalendar.Api
         }
 
         [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            var connectionString = Environment.GetEnvironmentVariable("EMAIL_CONNECTION_STRING");
+            var emailClient = new EmailClient(connectionString);
+
+            var emailMessage = new EmailMessage(
+              senderAddress: "DoNotReply@46f18bca-ca64-4724-afd7-58913db75f43.azurecomm.net",
+              content: new EmailContent("Test Email")
+              {
+                  PlainText = "Hello world via email.",
+                  Html = @"
+		<html>
+			<body>
+				<h1>Hello world via email.</h1>
+			</body>
+		</html>"
+              },
+              recipients: new EmailRecipients(new List<EmailAddress> { new EmailAddress("honzaklicpera@gmail.com") }));
+
+
+
+            EmailSendOperation emailSendOperation = await emailClient.SendAsync(WaitUntil.Completed, emailMessage);
+
+            _logger.LogInformation(connectionString);
             return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
