d8ae167623a5c57a1865f4e92cdcdb104ed701dd
Jan Klicpera <honza.klicpera@gmail.com>
2024-12-02 20:45:26 +0100

    Add API support

--- KlarkaAdventCalendar.Api/Program.cs
+++ KlarkaAdventCalendar.Api/Program.cs
@@ -3,22 +3,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var configuration = new ConfigurationBuilder()
-        .AddEnvironmentVariables()
-        .AddCommandLine(args)
-        .AddJsonFile("appsettings.json")
-        .Build();
-
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-        services.AddScoped(sp => new HttpClient
-        {
-            BaseAddress = new Uri(configuration["API_Base"])
-        });
     })
     .Build();
 
